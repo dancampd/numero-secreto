19e71a6127f13abd1a3c1c119fab6fdc55aaabad
Daniel Campos <65098409+dancampd@users.noreply.github.com>
2024-03-11 13:09:03 -0600

    Update "P" line 21.js
    Update paragraph, line 21, add "en"

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ function verificarIntento() {
     let numeroDeUsuario = parseInt(document.getElementById("valorUsuario").value);
     
     if (numeroDeUsuario === numeroSecreto) {
-        asignarTextoElemento("p", `Acertaste el número ${intentos} ${(intentos === 1) ? "vez" : "veces"}`);
+        asignarTextoElemento("p", `Acertaste el número en ${intentos} ${(intentos === 1) ? "vez" : "veces"}`);
         document.getElementById("reiniciar").removeAttribute("disabled");
     } else {
         if (numeroDeUsuario > numeroSecreto) {
